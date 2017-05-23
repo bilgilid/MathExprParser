b9b1274d7259813ca280bbc7bbe20da5336266ee
Deniz Bilgili <dnbilgili@gmail.com>
2017-05-24 00:06:14 +0300

    Add files via upload

---
+++ math_expr_parser.cpp
@@ -0,0 +1,454 @@
+#include "math_expr_parser.h"
+
+void MathExprParser::m_make_rpn() {
+	/*
+		Converts infit notation to reverse polish notation (RPN).
+	*/
+
+	if(m_inputExpr.empty()) throw BAD_INIT();
+
+	// necessary containers
+	std::stack<std::string> operatorStack;
+	std::queue<std::string> outputQueue;
+
+	// clear whitespaces from the input expression
+	std::istringstream iss(m_inputExpr);
+	std::string inputExprNoWS;
+	std::string token;
+
+	while(iss >> token) inputExprNoWS.append(token);
+	//
+
+	auto it = inputExprNoWS.cbegin();
+	auto itBegin = inputExprNoWS.cbegin();
+
+	while(it != inputExprNoWS.cend()) {
+		if(*it == '\'') {
+			// ' is the variable identifier character.
+			// pass if ' is encountered.
+			it++;
+			continue;
+		}
+
+		if(m_isOperator(it, itBegin)) {
+			// pop all operators on the top of the operator stack with greater
+			// precedence than (or equal to) this operator and put them in the 
+			// output queue
+			// Note: Functions have higher precedence than all operators
+			while(
+				!operatorStack.empty() && 
+					(m_precendence(operatorStack.top()) >= m_precendence(*it))
+				) {
+				std::string op = operatorStack.top();
+				outputQueue.push(op);
+				operatorStack.pop();
+			}
+
+			// finally, push this operator to the operator stack
+			operatorStack.push(std::string(1, *it));
+			it++;
+		}
+		else if(*it == '(') {
+			operatorStack.push(std::string(1, *it));
+			it++;
+		}
+		else if(*it == ')') {
+			// pop all operators from the operator stack until the top element
+			// is a left parenthesis and put the popped operators in the output
+			// queue. Discard the right parenthesis
+			while(!operatorStack.empty() && operatorStack.top() != "(") {
+				std::string op = operatorStack.top();
+				outputQueue.push(op);
+				operatorStack.pop();
+			}
+
+			// finally, pop the left parenthesis from the operator stack and
+			// discard it
+			// !! check if the operator stack is empty. if it is, there's an
+			//    unclosed right parenthesis
+			if(operatorStack.empty()) throw UNCLOSED_RPARENTHESIS();
+
+			operatorStack.pop();
+			it++;
+		}
+		else if(m_isNumber(it, itBegin)) {
+			std::string parsedNumber; // can be real integer or decimal number
+
+			while(it != inputExprNoWS.cend() && m_isNumber(it, itBegin)) {
+				parsedNumber.push_back(*it++);
+			}
+
+			outputQueue.push(parsedNumber);
+		}
+		else {
+			std::string parsedFunction;
+
+			while(it != inputExprNoWS.cend() && *it != '(') {
+				parsedFunction.push_back(*it++);
+			}
+
+			operatorStack.push(parsedFunction);
+		}
+	}
+
+	// Pop all items from the operator stack and push them to the output queue
+	while(!operatorStack.empty()) {
+		std::string op = operatorStack.top();
+		outputQueue.push(op);
+		operatorStack.pop();
+	}
+
+	std::string returnRPN;
+
+	while(!outputQueue.empty()) {
+		returnRPN.append(outputQueue.front());
+		returnRPN.push_back(' ');
+		outputQueue.pop();
+	}
+
+	// test for unclosed left parenthesis
+	// If there's an unclosed left parenthesis in the beginning of the expr.,
+	// it shows up as the last element of the RPN. Check for that first
+	auto checkIterator = returnRPN.cend();
+	if(*(checkIterator-2) == '(')
+		throw UNCLOSED_LPARENTHESIS();
+
+	// check for the rest of the RPN. In the infix notation, the unclosed left 
+	// parenthesis is found after the operator/function which comes after that 
+	// left parenthesis in the RPN
+	std::istringstream issTestLParenthesis(returnRPN);
+	std::string tokenTestLParenthesis;
+
+	while(issTestLParenthesis >> tokenTestLParenthesis) {
+		if(tokenTestLParenthesis == "(") {
+			issTestLParenthesis >> tokenTestLParenthesis;
+			throw UNCLOSED_LPARENTHESIS();
+		}
+	}
+
+	m_rpn = returnRPN;
+
+}
+
+double MathExprParser::m_calc_rpn() {
+
+/*
+	Calculates the expression given in reverse polish notation, and returns the 
+	result as double.
+*/
+
+	if(m_rpn.empty()) throw BAD_RPN();
+
+	std::stack<double> numberStack;
+	
+	std::istringstream iss(m_rpn);
+	std::string token;
+
+	try {
+		while(iss >> token) {
+			if(m_isNumber(token)) {
+				numberStack.push(std::stod(token));
+			}
+			else if(m_isOperator(token)) {
+				if(numberStack.empty()) throw INPUT_EXPR_SYNTAX_ERROR();
+				double rVal = numberStack.top();
+				numberStack.pop();
+
+				if(numberStack.empty()) throw INPUT_EXPR_SYNTAX_ERROR();
+				double lVal = numberStack.top();
+				numberStack.pop();
+
+				char op = token.c_str()[0];
+
+				double operatorCalcResult = m_calc_operator(lVal, rVal, op);
+				numberStack.push(operatorCalcResult);
+
+			}
+			else if(auto func = m_isFunction(token)) {
+				if(numberStack.empty()) throw INPUT_EXPR_SYNTAX_ERROR();
+				double val = numberStack.top();
+				numberStack.pop();
+
+				double functionCalcResult = m_calc_function(val, func);
+				numberStack.push(functionCalcResult);
+			}
+			else {
+				throw INPUT_EXPR_SYNTAX_ERROR();
+			}
+		}
+	}
+	catch(const INPUT_EXPR_SYNTAX_ERROR& e) {
+		throw;
+	}
+
+	if(numberStack.empty()) throw INPUT_EXPR_SYNTAX_ERROR();
+
+	return numberStack.top();
+}
+
+double MathExprParser::calculate(vector_Var& variables) {
+
+	for(size_t i = 0; i < variables.size(); i++) {
+		std::string variableName = variables[i].name;
+
+		// whether the variable is known or not should be checked before
+		// anything else
+		if(!m_variableExists(variableName))
+			throw UNKNOWN_VARIABLE(variableName);
+		//
+
+		std::string modifiedVarName = "'" + variableName + "'";
+		std::string variableValue = std::to_string(variables[i].value);
+
+		size_t pos;
+
+		while((pos = m_inputExpr.find(modifiedVarName)) != std::string::npos) {
+			size_t oldVarSize = modifiedVarName.size();
+			m_inputExpr.replace(pos, oldVarSize, variableValue);
+		}
+	}
+
+	double result;
+
+	try {
+		m_make_rpn();
+		result = m_calc_rpn();
+	}
+	catch(const BAD_INIT& e) {
+		throw;
+	}
+	catch(const BAD_RPN& e) {
+		throw;
+	}
+	
+	return result;
+
+}
+
+bool MathExprParser::m_isOperator(const std::string::const_iterator& it, 
+	const std::string::const_iterator& itBegin) const {
+	/*
+		it:      The string iterator iterating over the input expression.
+		itBegin: The iterator pointing at the end of the input expression.
+	*/
+
+	char token = *it;
+
+	switch(token) {
+		case '+':
+		case '-':
+			// "-" is counted as number and not operator if:
+			//	1. it is the first token in the input expression
+			//	2. it comes immediately after a left parenthesis
+			//	3. it comes after an operator, including "-"
+			if(it == itBegin) return false;
+			if(*(it-1) == '(') return false;
+			if(m_isOperator(it-1, itBegin)) return false;
+			return true;
+		case '*':
+		case '/':
+		case '^':
+			return true;
+		default:
+			return false;
+	}
+
+}
+
+bool MathExprParser::m_isOperator(const std::string& token) const {
+	/*
+		Checks if token, which is taken from the number stack, is an operator.
+	*/
+
+	if(token == "+") return true;
+	if(token == "-") return true;
+	if(token == "*") return true;
+	if(token == "/") return true;
+	if(token == "^") return true;
+	else return false;
+
+}
+
+bool MathExprParser::m_isNumber(const std::string::const_iterator& it,
+	const std::string::const_iterator& itBegin) const {
+	/*
+	it     : the string iterator iterating over the input expression.
+	itBegin: the iterator pointing at the end of the input expression.
+	*/
+
+	char token = *it;
+	
+	switch(token) {
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+		case '.': // for floating-point numbers
+			return true;
+		case '-':
+			// "-" is counted as number and not operator if:
+			//	1. it is the first token in the input expression
+			//	2. it comes immediately after a left parenthesis
+			//	3. it comes after an operator, including "-"
+			if(it == itBegin) return true;
+			if(*(it-1) == '(') return true;
+			if(m_isOperator(it-1, itBegin)) return true;
+			return false;
+		default:
+			return false;
+	}
+
+}
+
+bool MathExprParser::m_isNumber(const std::string& token) const {
+	/*
+		Checks if token, which is taken from the number stack, is a number.
+	*/
+
+	try {
+		std::stod(token);
+	}
+	catch(const std::invalid_argument& e) {
+		return false;
+	}
+
+	return true;
+
+}
+
+bool MathExprParser::m_variableExists(const std::string& varName) const {
+
+/*
+	Checks if the varible varName exists in the input expression.
+*/
+
+	if(m_inputExpr.empty()) throw BAD_INIT();
+
+	std::string modifiedVarName = "'" + varName + "'";
+
+	if(m_inputExpr.find(modifiedVarName) == std::string::npos)
+		return false;
+		
+	return true;
+
+}
+
+FUNCTION MathExprParser::m_isFunction(const std::string& token) const {
+
+	if(token == "LOG" || token == "log") return FUNCTION::LOG;
+	if(token == "LOG10" || token == "log10") return FUNCTION::LOG10;
+	if(token == "SIN" || token == "sin") return FUNCTION::SIN;
+	if(token == "COS" || token == "cos") return FUNCTION::COS;
+	if(token == "TAN" || token == "tan") return FUNCTION::TAN;
+	if(token == "COT" || token == "cot") return FUNCTION::COT;
+	if(token == "ASIN" || token == "asin") return FUNCTION::ASIN;
+	if(token == "ACOS" || token == "acos") return FUNCTION::ACOS;
+	if(token == "ATAN" || token == "atan") return FUNCTION::ATAN;
+	if(token == "ATAN2" || token == "atan2") return FUNCTION::ATAN2;
+	if(token == "ACOT" || token == "acot") return FUNCTION::ACOT;
+	if(token == "DEG" || token == "deg") return FUNCTION::DEG;
+	if(token == "RAD" || token == "rad") return FUNCTION::RAD;
+	if(token == "SQRT" || token == "sqrt") return FUNCTION::SQRT;
+	if(token == "EXP" || token == "exp") return FUNCTION::EXP;
+	if(token == "ABS" || token == "abs") return FUNCTION::ABS;
+	else return FUNCTION::NONE;
+
+}
+
+int MathExprParser::m_precendence(const char& op) const {
+
+	switch(op) {
+		case '+':
+		case '-':
+			return 2;
+		case '*':
+		case '/':
+		case '%':
+			return 3;
+		case '^':
+			return 4;
+		default:
+			return 1;
+	}
+
+}
+
+int MathExprParser::m_precendence(const std::string& op) const {
+
+	if(op == "+" || op == "-") return 2;
+	if(op == "*" || op == "/" || op == "%") return 3;
+	if(op == "^") return 4;
+	if(m_isFunction(op)) return 5;
+	else return 1;
+
+}
+
+double MathExprParser::m_calc_operator(double lVal, double rVal,
+	char op) const {
+
+	switch(op) {
+		case '+':
+			return lVal + rVal;
+		case '-':
+			return lVal - rVal;
+		case '*':
+			return lVal * rVal;
+		case '/':
+			return lVal / rVal;
+		case '%':
+			return std::fmod(lVal, rVal);
+		case '^':
+			return std::pow(lVal, rVal);
+		default:
+			throw INPUT_EXPR_SYNTAX_ERROR();
+	}
+
+}
+
+double MathExprParser::m_calc_function(double val, FUNCTION func) const {
+
+	switch(func) {
+		case NONE:
+			throw INPUT_EXPR_SYNTAX_ERROR();
+		case LOG:
+			return std::log(val);
+		case LOG10:
+			return std::log10(val);
+		case SIN:
+			return std::sin(val);
+		case COS:
+			return std::cos(val);
+		case TAN:
+			return std::tan(val);
+		case COT:
+			return 1/std::tan(val);
+		case ASIN:
+			return std::asin(val);
+		case ACOS:
+			return std::acos(val);
+		case ATAN:
+			return std::atan(val);
+		// case ATAN2: implement later
+		case ACOT:
+			return std::atan(1/val);
+		case DEG:
+			return (val/(2*PI))*360;
+		case RAD:
+			return (val/360)*2*PI;
+		case SQRT:
+			return std::sqrt(val);
+		case EXP:
+			return std::exp(val);
+		case ABS:
+			return std::abs(val);
+		default:
+			throw INPUT_EXPR_SYNTAX_ERROR();
+	}
+
+}
---
+++ math_expr_parser.h
@@ -0,0 +1,229 @@
+/*
+Program:        Math Expression Parser v1.0.
+Author:         Deniz Bilgili
+		        Technical University of Istanbul, 
+				Department of Mechanical Engineering
+Date published: 05.2017
+
+This code is shared publicly, under MIT License. To see the license, please 
+refer to the LICENSE.txt file.
+
+Please do not delete this section.
+*/
+
+
+#ifndef MATH_EXPR_PARSER_H
+#define MATH_EXPR_PARSER_H
+#define PI 3.14159265358979323846  /* pi */
+
+#include <string>
+#include <stack>
+#include <queue>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <exception>
+
+
+struct Var {
+
+	std::string name;
+	double value;
+
+};
+
+typedef std::vector<Var> vector_Var;
+
+enum FUNCTION {
+
+	NONE = 0,
+	LOG,
+	LOG10,
+	SIN,
+	COS,
+	TAN,
+	COT,
+	ASIN,
+	ACOS,
+	ATAN,
+	ATAN2, // not supported yet
+	ACOT,
+	DEG,
+	RAD,
+	SQRT,
+	EXP,
+	ABS
+
+};
+
+class UNCLOSED_LPARENTHESIS: public std::exception {
+
+public:
+	virtual const char* what() const noexcept {
+		return "An unclosed left parenthesis was found in the input "
+			"expression.";
+	}
+
+};
+
+class UNCLOSED_RPARENTHESIS: public std::exception {
+
+public:
+	virtual const char* what() const noexcept {
+		return "An unclosed right parenthesis was found in the input "
+			"expression.";
+	}
+
+};
+
+class INPUT_EXPR_SYNTAX_ERROR: public std::exception {
+
+public:
+	virtual const char* what() const noexcept {
+		return "Syntax error in the input expression.";
+	}
+
+};
+
+class BAD_INIT: public std::exception {
+
+public:
+	virtual const char* what() const noexcept {
+		return "Bad initialization of MathExprParser object. Input expression "
+			"was not set.";
+	}
+
+};
+
+class BAD_RPN: public std::exception {
+
+public:
+	virtual const char* what() const noexcept {
+		return "The reverse polish notation (RPN) sent to calculation "
+			"is empty.";
+	}
+
+};
+
+class UNKNOWN_VARIABLE: public std::exception {
+
+public:
+	UNKNOWN_VARIABLE(const std::string& varName) {
+		m_returnMessage = "Variable not found in the input expression: " +
+			varName;
+	}
+
+	virtual const char* what() const noexcept {
+		return m_returnMessage.c_str();
+	}
+
+private:
+	std::string m_returnMessage;
+
+};
+
+class MathExprParser {
+
+/*
+	Mathematical expression parser and calculator object for real-valued input.
+	Works only with real numbers and returns real numbers. Uses the Shunting
+	Yard Algorithm.
+
+	Steps for usage:
+
+	A. Without variables
+		- Have the mathematical expression you want to solve stored in a string
+		  in infix notation
+
+			e.g. std::string expr = "-12.4 + exp(sin(rad(68))) * log10(96)";
+
+		- Pass the expression to the constructor and create the parser object
+			
+			e.g. MathExprParser mep(expr);
+
+		- Call calculate() to parse and calculate the expression and save it in
+		  a double.
+
+			e.g. double result = mep.calculate();
+
+	B. With variables
+		- Have the mathematical expression you want to solve stored in a string 
+		  in infix notation. Use ' character before and after variable names to
+		  mark variables.
+
+			e.g. std::string expr = "-12.4 + exp(sin(rad('x'))) * log10('y')";
+				 x and y are variables.
+		
+		- Pass the expression to the constructor and create the parser object
+
+			e.g. MathExprParser mep(expr);
+
+		- Create Var objects for each variable in the input expression and
+		  store all Var objects in a vector
+
+			e.g. Var x{"x", 1.13};
+				 Var y{"y", 22.8};
+
+				 vector_Var vars;
+				 vars.push_back(x);
+				 vars.push_back(y);
+
+			!! In VAR objects, variable names must NOT have ' characters. ' are
+			   used only in the input expression in order to mark variables.
+
+		- Call calculate() and pass the vector of Vars to parse and calculate 
+		  the expression and save it in a double.
+
+			e.g. double result = mep.calculate(vars);
+
+	Notes:
+		- Function names can be all lowercase or all uppercase.
+
+
+	Limitations:
+		- Supported operators: +, -, *, /, %, ^
+		- Supported functions: Given under enum FUNCTION
+		- Only real numbers are supported
+		- Results are returned only as double
+*/
+
+public:
+	MathExprParser(const std::string& inputExpr): m_inputExpr(inputExpr) {}
+
+	const std::string& input_expr() const { return m_inputExpr; }
+	const std::string& rpn() const { return m_rpn; }
+
+	double calculate(vector_Var& variables = vector_Var());
+
+	void set_input_expr(const std::string& input) { m_inputExpr = input; }
+
+	virtual ~MathExprParser() {}
+
+private:
+	std::string m_inputExpr;
+	std::string m_rpn;
+
+	bool m_isOperator(const std::string::const_iterator& it,
+		const std::string::const_iterator& itBegin) const;
+	bool m_isOperator(const std::string& token) const;
+
+	bool m_isNumber(const std::string::const_iterator& it,
+		const std::string::const_iterator& itBegin) const;
+	bool m_isNumber(const std::string& token) const;
+
+	bool m_variableExists(const std::string& varName) const;
+
+	FUNCTION m_isFunction(const std::string& token) const;
+
+	int m_precendence(const char& op) const;
+	int m_precendence(const std::string& op) const;
+
+	double m_calc_operator(double lVal, double rVal, char op) const;
+	double m_calc_function(double val, FUNCTION func) const;
+
+	void m_make_rpn();
+	double m_calc_rpn();
+
+};
+
+#endif // !MATH_EXPR_PARSER_H
